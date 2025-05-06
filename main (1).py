5dd306090448bd453127f4a964dfb020d7b1985f
Prince Kumar <siharamkumar@gmail.com>
2025-05-06 22:32:08 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,8 @@
+n=(input("Please enter your name :-"))
+a=int(input("Enter your age :-"))
+if(a>=18):
+    print("Congratulations,",n)
+    print("you can participate in voting. ")
+else:
+    print("sorry,",n)
+    print("you can't participate in voting.")
